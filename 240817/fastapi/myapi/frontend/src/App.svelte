3440c3d4d643bfd2af1c8e51b37bc704e6ee4d02
soyoungshin <ccvv0215@gmail.com>
2024-08-16 17:08:20 +0000

    fastapi study

---
+++ 240817/fastapi/myapi/frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+// @ts-nocheck
+
+  let message;
+
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
+</script>
+
+<h1>{message}</h1>
